62e9653bbfc5005c24ee336cdeeb38a035f624ae
Noobgam <shessmaster12@gmail.com>
2022-04-29 00:45:48 +0300

    pushing streaming grpc

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import asyncio
 import json
 import time
+import threading
 import os
 
 import discord
-import grpc
 import requests
 from discord.ext import commands
 
@@ -29,14 +29,13 @@ ID_FOLDER = "b1g5pq4lh1ktoijrb9u1"
 kostil = True
 
 cred = grpc.ssl_channel_credentials()
-channel = grpc.secure_channel('stt.api.cloud.yandex.net:443', cred)
+channel = grpc.aio.secure_channel('stt.api.cloud.yandex.net:443', cred)
 stub = stt_service_pb2_grpc.SttServiceStub(channel)
-speaking_queue = asyncio.queues.Queue()
 user_queue = []
 
 specification = stt_service_pb2.RecognitionSpec(
     language_code='ru-RU',
-    profanity_filter=True,
+    profanity_filter=False,
     model='general',
     partial_results=True,
     audio_encoding='LINEAR16_PCM',
@@ -44,35 +43,37 @@ specification = stt_service_pb2.RecognitionSpec(
 )
 streaming_config = stt_service_pb2.RecognitionConfig(specification=specification, folder_id=ID_FOLDER)
 config_msg = stt_service_pb2.StreamingRecognitionRequest(config=streaming_config)
-
-def recognize_grpc(audio_bytes):
-    # Establish a connection with the server.
-
-    msg = stt_service_pb2.StreamingRecognitionRequest(audio_content=audio_bytes.read())
-    send_msg([config_msg, msg])
+speaking_queue = asyncio.queues.Queue()
 
 
-def send_msg(msgs):
+async def generate_from_queue(queue):
+    yield config_msg
+    while True:
+        yield await queue.get()
+
+
+async def communicate_queue(gen):
     global stub
 
     it = stub.StreamingRecognize(
-        iter(msgs),
+        gen,
         metadata=(('authorization', 'Bearer %s' % IAM_TOKEN),)
     )
     # Process server responses and output the result to the console.
     try:
-        for r in it:
-            print(r)
+        while True:
+            r = await it.read()
+            #print(r)
             try:
                 print('Start chunk: ')
                 for alternative in r.chunks[0].alternatives:
                     print('alternative: ', alternative.text)
                 print('Is final: ', r.chunks[0].final)
-                print('')
+                #print('')
             except LookupError:
                 print('Not available chunks')
-    except grpc._channel._Rendezvous as err:
-        print('Error code %s, message: %s' % (err._state.code, err._state.details))
+    except Exception as err:
+        print(err)
 
 
 @bot.event
@@ -84,24 +85,25 @@ async def on_ready():
 
 @bot.command()
 async def start(ctx):
-    global kostil
-    # print(dir(bot.get_all_channels()))
-    # channel = discord.utils.get(server.channels, name="Channel_name_here", type="ChannelType.voice")
-    #if not ctx.voice_client.is_connected():
-    if kostil:
-        await ctx.author.voice.channel.connect()
-        kostil = False
+    await ctx.author.voice.channel.connect()
+    asyncio.ensure_future(
+        communicate_queue(generate_from_queue(speaking_queue))
+    )
+    await serve(ctx)
     # Connect to the voice channel of the author
 
+
+async def serve(ctx):
     ctx.voice_client.start_recording(discord.sinks.PCMSink(), finished_callback, ctx) # Start the recording
-    print("Recording...")
+    #print("Recording...")
     await asyncio.sleep(0.2)
     ctx.voice_client.stop_recording()
-    print("Stopped!")
+    #print("Stopped!")
+
 
 
 async def finished_callback(sink, ctx):
-    print(sink.audio_data)
+    #print(sink.audio_data)
     # print(dir(sink.audio_data))
     # Here you can access the recorded files:
     recorded_users = [
@@ -111,21 +113,20 @@ async def finished_callback(sink, ctx):
     files = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]
     for user_id, audio in sink.audio_data.items():
         #speaking_queue.put_nowait(audio.file)
-        recognize_grpc(audio.file)
+        msg = stt_service_pb2.StreamingRecognitionRequest(audio_content=audio.file.read())
+        speaking_queue.put_nowait(msg)
         # file = discord.File(audio.file, f"{user_id}.{sink.encoding}")
         # print(dir(audio.file))
         # text = recognize(audio.file).get('result', '').lower()
         # if 'тимыч' in text:
         #     member_to_kick = ctx.voice_client.channel.guild.get_member(user_id)
         #     await member_to_kick.edit(voice_channel=None)
-    await start(ctx)
-
+    await serve(ctx)
 
 @bot.command()
 async def stop(ctx):
     await ctx.voice_client.disconnect()
     print("Stopped!")
-
 
 if __name__ == "__main__":
     token = os.environ['BOT_TOKEN']
